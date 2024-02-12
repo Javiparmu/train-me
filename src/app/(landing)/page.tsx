f8170f12513fbfb1d2f7b01f49ac0bc2e951034f
javiparmu <mrjavistinson@gmail.com>
2024-02-12 22:45:33 +0100

    feat: landing buttons and logo

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,9 +1,13 @@
 import { HeroSection } from '@/components/hero-section';
+import Image from 'next/image';
 import React from 'react';
 
 const LandingPage = () => {
   return (
     <main>
+      <nav className="flex items-center justify-start py-4 px-12">
+        <Image alt="Hero Image" src="/images/logo.png" width={329} height={152} className="w-32" />
+      </nav>
       <HeroSection />
     </main>
   );
